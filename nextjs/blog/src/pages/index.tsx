0b212aeb41ffe1cf9e73eb59e7eb82454ef2275f
Leonardo Capitani Pansonato <leopansonato@gmail.com>
2025-09-01 23:53:46 -0300

    feat: header component

--- nextjs/blog/src/pages/index.tsx
+++ nextjs/blog/src/pages/index.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -18,8 +17,7 @@ export default function Home() {
 			className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
 `}
 		>
-			<h2>Hello World!</h2>
-         <Button>Button</Button>
+         <Header />
 		</div>
 	);
 }
