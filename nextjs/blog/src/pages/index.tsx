bc6a7d28384dbf1d0c45dac2cabdc1abd29c9e94
Leonardo Capitani Pansonato <leopansonato@gmail.com>
2025-09-07 21:33:16 -0300

    feat: features section

--- nextjs/blog/src/pages/index.tsx
+++ nextjs/blog/src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { HeroSection } from "@/components";
+import { FeatureSection, HeroSection } from "@/components";
 
 export default function Home() {
 	return (
 		<>
 			<article className="flex flex-col">
 				<HeroSection />
+            <FeatureSection />
 			</article>
 		</>
 	);
